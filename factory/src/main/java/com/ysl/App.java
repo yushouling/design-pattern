70ffbecdba64bc69083c49c2b2c4a83687482648
yusl <yusl@mti-sh.cn>
2021-03-02 18:02:47 +0800

    优化pom

--- factory/src/main/java/com/ysl/App.java
+++ factory/src/main/java/com/ysl/App.java
@@ -3,6 +3,7 @@ package com.ysl;
 import com.ysl.com.ysl.Phone;
 import com.ysl.com.ysl.PhoneFactory;
 import com.ysl.com.ysl.factory.ApplePhoneFactory;
+import com.ysl.com.ysl.factory.HuaweiPhoneFactory;
 
 /**
  * 工厂模式
@@ -13,5 +14,9 @@ public class App
         PhoneFactory applePhoneFactory = new ApplePhoneFactory();
         Phone iphone = applePhoneFactory.createPhone();
         iphone.printBrand();
+
+        PhoneFactory huaweiPhoneFactory = new HuaweiPhoneFactory();
+        Phone huaweiPhone = huaweiPhoneFactory.createPhone();
+        huaweiPhone.printBrand();
     }
 }
